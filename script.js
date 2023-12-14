5c7bb9b2677b42674e06e6fa35a5aacc05be0e92
Restioktapiani06 <144655262+Restioktapiani06@users.noreply.github.com>
2023-12-14 21:12:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+console.log("helo")
+var soal1 = prompt("1 + 1 :")
+var poin = 0
+
+if (soal1 == 2) {
+    poin += 1
+    alert(`jawaban anda benar, poin anda sekarang ${poin}`)
+    var soal2 = prompt("ibu kota indonesia adalah :")
+    if (soal2 == "jakarta"){
+        poin += 1
+        alert(`jawaban anda benar, poin anda sekarang ${poin}`)
+        var soal3 = prompt("2 x 5 :")
+        if (soal3 == 10){
+            poin += 1
+            alert(`jawaban anda benar,poin anda sekarang ${poin}`)
+        }else {
+            poin += 0
+            alert(`jawaban salah poin anda adalah ${poin}`)
+        }
+    }else {
+        poin += 0
+        alert(`jawaban salah poin anda adalah ${poin}`)
+    }
+}else {
+    poin += 0
+    alert(`jawaban salah poin anda adalah ${poin}`)
+}
